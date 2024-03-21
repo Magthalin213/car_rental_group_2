97b64b790e560a946184d21495b7f7ae9d5588fe
MadhavanI <madhavanilankovan@gmail.com>
2024-03-21 17:13:55 +0530

    Payment method is implemented

--- src/com/dao/PaymentDao.java
+++ src/com/dao/PaymentDao.java
@@ -1,5 +1,11 @@
 package com.dao;
 
-public class PaymentDao {
+import com.model.Payment;
 
+import java.sql.SQLException;
+import java.util.List;
+
+public interface PaymentDao {
+    void recordPayment(Payment payment) throws SQLException;
+    List<Payment> getPaymentsByLeaseId(int leaseId) throws SQLException;
 }
--- src/com/model/Payment.java
+++ src/com/model/Payment.java
@@ -1,5 +1,63 @@
 package com.model;
 
-public class Payment {
+import java.time.LocalDate;
 
+public class Payment {
+	private int id;
+	private Lease lease;
+	private LocalDate paymentDate;
+	private double amount;
+	
+	public Payment() {
+		super();
+		
+	}
+
+	public Payment(int id, Lease lease, LocalDate paymentDate, double amount) {
+		super();
+		this.id = id;
+		this.lease = lease;
+		this.paymentDate = paymentDate;
+		this.amount = amount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Lease getLease() {
+		return lease;
+	}
+
+	public void setLease(Lease lease) {
+		this.lease = lease;
+	}
+
+	public LocalDate getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDate paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	
+
+	
+	
+	
+	
+	
 }
